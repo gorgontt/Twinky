f5a76fc792871977ce03c952e4005fcabe28f543
gorgontt <166929134+gorgontt@users.noreply.github.com>
2024-05-14 23:58:11 +0300

    bottomsheetdialog crash

--- app/src/main/java/com/example/twinky/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/twinky/ui/profile/ProfileFragment.kt
@@ -1,28 +1,62 @@
 package com.example.twinky.ui.profile
 
+import android.app.ProgressDialog
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.twinky.R
-import com.example.twinky.Registration.LoginActivity
+import com.google.android.gms.tasks.Task
+import com.google.android.material.button.MaterialButton
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import com.rey.material.app.BottomSheetDialog
 import com.rey.material.widget.TextView
+import java.text.DateFormat
+import java.util.Calendar
+import java.util.Objects
 
 
 class ProfileFragment : Fragment() {
 
     private lateinit var dialog: BottomSheetDialog
+    private lateinit var changeAvatar : ImageView
+    private lateinit var saveButton: MaterialButton
+
+    private lateinit var Description: String
+    private lateinit var Pname: String
+    private lateinit var Price: String
+    private lateinit var saveCurrentDate: String
+    private lateinit var saveCurrentTime: String
+    private lateinit var productRandomKey: String
+    private lateinit var downloadImageUrl: String
+    private lateinit var categoryName: String
+
+    private lateinit var productDescription: EditText
+    private lateinit var productName: EditText
+    private lateinit var productPrice: EditText
+
+    private lateinit var ImageUri: Uri
+    private val GALLERYPICK = 1
+    private lateinit var loadingBar: ProgressDialog
+
+
+    private lateinit var ProductImageRef: StorageReference
+    private lateinit var ProductsRef: DatabaseReference
 
     companion object {
         fun newInstance() = ProfileFragment()
@@ -72,11 +106,127 @@ class ProfileFragment : Fragment() {
 
     private fun showBottomSheet() {
         val dialogView = layoutInflater.inflate(R.layout.bottom_sheet_settings, null)
+        changeAvatar = dialogView.findViewById(R.id.settings_frag_imgV)
+        saveButton = dialogView.findViewById(R.id.settings_frag_btn_save)
+        productDescription = dialogView.findViewById(R.id.settings_frag_ET_phone)
+        productName = dialogView.findViewById(R.id.settings_frag_ET_name)
+        productPrice = dialogView.findViewById(R.id.settings_frag_ET_password)
+
+
+
+        changeAvatar.setOnClickListener {
+            openGallery()
+        }
+
+        saveButton.setOnClickListener {
+            validateProductData()
+        }
+
+
         dialog = BottomSheetDialog(activity, R.style.BottomSheetDialogTheme)
         dialog.setContentView(dialogView)
         dialog.show()
     }
 
+    private fun openGallery() {
+        val galleryIntent = Intent()
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT)
+        galleryIntent.setType("image/*")
+        startActivityForResult(galleryIntent, GALLERYPICK)
+    }
+
+    private fun validateProductData() {
+        Description = productDescription.getText().toString()
+        Pname = productName.getText().toString()
+        Price = productPrice.getText().toString()
+        if (ImageUri == null) {
+            Toast.makeText(activity, "Добавьте фото", Toast.LENGTH_SHORT).show()
+        } else if (TextUtils.isEmpty(Description)) {
+            Toast.makeText(activity, "Добавьте описание!", Toast.LENGTH_SHORT).show()
+        } else if (TextUtils.isEmpty(Price)) {
+            Toast.makeText(activity, "Добавьте цену!", Toast.LENGTH_SHORT).show()
+        } else if (TextUtils.isEmpty(Pname)) {
+            Toast.makeText(activity, "Добавьте название!", Toast.LENGTH_SHORT).show()
+        } else {
+            StoreProductInformation()
+        }
+    }
+
+    private fun StoreProductInformation() {
+        loadingBar?.setTitle("Загрузка")
+        loadingBar?.setMessage("Пожалуйста, подождите")
+        loadingBar?.setCanceledOnTouchOutside(false)
+        loadingBar?.show()
+        val calendar = Calendar.getInstance()
+        val currentDate = DateFormat.getDateInstance()
+        saveCurrentDate = currentDate.format(calendar.time)
+        val currentTime = DateFormat.getTimeInstance()
+        saveCurrentTime = currentTime.format(calendar.time)
+        productRandomKey = saveCurrentDate + saveCurrentTime
+        val filePath: StorageReference =
+            ProductImageRef.child(ImageUri.lastPathSegment + productRandomKey + ".jpg")
+        val uploadTask = filePath.putFile(ImageUri)
+        uploadTask.addOnFailureListener { e: Exception ->
+            loadingBar.dismiss()
+            val message = e.toString()
+            Toast.makeText(activity, "Ошибка $message", Toast.LENGTH_SHORT).show()
+        }.addOnSuccessListener { taskSnapshot: UploadTask.TaskSnapshot? ->
+            Toast.makeText(activity, "Изображение загружено", Toast.LENGTH_SHORT)
+                .show()
+            val uriTask =
+                uploadTask.continueWithTask<Uri?> { task: Task<UploadTask.TaskSnapshot?> ->
+                    if (!task.isSuccessful) {
+                        throw Objects.requireNonNull(task.exception)!!
+                    }
+                    downloadImageUrl = filePath.getDownloadUrl().toString()
+                    filePath.getDownloadUrl()
+                }
+                    .addOnCompleteListener { task: Task<Uri?> ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                activity,
+                                "Фото сохранено",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                            SaveProductInfoToDatabase()
+                        }
+                    }
+
+        }
+    }
+
+    private fun SaveProductInfoToDatabase() {
+        val productMap = HashMap<String, Any>()
+        productMap["pid"] = productRandomKey
+        productMap["date"] = saveCurrentDate
+        productMap["time"] = saveCurrentTime
+        productMap["description"] = Description
+        productMap["image"] = downloadImageUrl
+        productMap["category"] = categoryName
+        productMap["price"] = Price
+        productMap["productName"] = Pname
+        ProductsRef!!.child(productRandomKey).updateChildren(productMap)
+            .addOnCompleteListener { task: Task<Void?> ->
+                if (task.isSuccessful) {
+                    loadingBar!!.dismiss()
+//                    val homeIntent: Intent = Intent(
+//                        this@ProfileFragment,
+//                        AdminCategoryActivity::class.java
+//                    )
+//                    startActivity(homeIntent)
+                    Toast.makeText(activity, "Товар добавлен", Toast.LENGTH_LONG)
+                        .show()
+                } else {
+                    loadingBar!!.dismiss()
+                    val message =
+                        Objects.requireNonNull(task.exception)
+                            .toString()
+                    Toast.makeText(activity, "Ошибка: $message", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+    }
 
 
 }
