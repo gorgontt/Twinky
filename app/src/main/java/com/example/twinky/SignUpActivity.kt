75000ea3441fcd2d8b092449b6a6f0ec28067775
gorgontt <166929134+gorgontt@users.noreply.github.com>
2024-05-15 01:33:49 +0300

    SIGN UP xml page OK

--- app/src/main/java/com/example/twinky/SignUpActivity.kt
+++ app/src/main/java/com/example/twinky/SignUpActivity.kt
@@ -1,33 +1,20 @@
 package com.example.twinky
 
-import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        window.statusBarColor = Color.TRANSPARENT
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, SignUpActivity::class.java))
-            finish()
-        }, 2000)
-
     }
-
 }
